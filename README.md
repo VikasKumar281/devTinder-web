2734096982c627a295ef8ace367c7de77ecb381e
VikasKumar281 <vikaskumar280204@gmail.com>
2025-03-07 17:54:41 +0530

    tailwind + daisyUI + Navbar

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 - Created a Vite + React application
 - Remove unecessary code an create a Hello World app
 - Install Tailwind CSS
+- Install Daisy UI
+- Add NavBar component to App.jsx
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-
-import './App.css'
-
 function App() {
-
   return (
     <>
-      <h1 className=" text-3xl bg-yellow-300 text-red-700" >Hello World</h1>
+    
+     {/* Navbar Section */}
+       <div className="navbar bg-base-300 shadow-sm">
+         <div className="flex-1">
+           <a className="btn btn-ghost text-xl"> 👨‍💻 DevTinder</a>
+         </div>
+         <div className="flex gap-2">
+
+           {/* Profile Dropdown */}
+           <div className="dropdown dropdown-end mx-4">
+             <div 
+               tabIndex={0} 
+               role="button" 
+               className="btn btn-ghost btn-circle avatar"
+             >
+               <div className="w-10 rounded-full">
+                 <img
+                   alt="Tailwind CSS Navbar component"
+                   src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                 />
+               </div>
+             </div>
+             <ul
+               tabIndex={0}
+               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+             >
+               <li>
+                 <a className="justify-between">
+                   Profile
+                   <span className="badge">New</span>
+                 </a>
+               </li>
+               <li><a>Settings</a></li>
+               <li><a>Logout</a></li>
+             </ul>
+           </div>
+         </div>
+       </div>
+
+      {/* Main Content */}
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
