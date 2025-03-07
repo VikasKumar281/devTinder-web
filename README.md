9bf7aec7f796411aead5c30cd81e93eb8afab64c
VikasKumar281 <vikaskumar280204@gmail.com>
2025-03-07 16:27:26 +0530

    hello world

---
+++ README.md
@@ -0,0 +1,4 @@
+# DevTinder
+- Created a Vite + React application
+- Remove unecessary code an create a Hello World app
+- Install Tailwind CSS
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=" text-3xl bg-yellow-300 text-red-700" >Hello World</h1>
+    </>
+  )
+}
+
+export default App
