c08f28121b69765891ab2c08534e6b6f1700a949
VikasKumar281 <vikaskumar280204@gmail.com>
2025-03-10 16:54:49 +0530

    built logout feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,6 +9,8 @@ const Login = () => {
 
    const[emailId , setEmailId] = useState("vikaskumar280204@gmail.com");
    const[password , setPassword] = useState("Vikas@123");
+   const[error , setError] = useState("");
+
    const dispatch = useDispatch();
    const navigate = useNavigate();
 
@@ -29,7 +31,7 @@ const Login = () => {
        return navigate("/");
      }
      catch(err){
-      console.error(err);
+      setError(err?.response?.data||"Something went wrong!!");
      }
    };
 
@@ -65,6 +67,7 @@ const Login = () => {
               </label>
             </div>
 
+            <p className="text-red-400">{error}</p>
             <div className="card-actions justify-center m-2">
               <button className="btn btn-primary" onClick={handleLogin}>Login</button>
             </div>
